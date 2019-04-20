ccdfaec2530834cd8dc74b05f36da75833cf56b2
Dave Mielke <Dave@Mielke.cc>
2019-04-20 09:50:05 -0400

    Finish the radio's book player.

--- android/radio/src/cc/mielke/dave/android/radio/BookPlayer.java
+++ android/radio/src/cc/mielke/dave/android/radio/BookPlayer.java
@@ -1,6 +1,11 @@
 package cc.mielke.dave.android.radio;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.io.File;
+
+import java.util.Set;
+import java.util.HashSet;
 
 public class BookPlayer extends FilePlayer {
   public BookPlayer () {
@@ -16,6 +21,46 @@ public class BookPlayer extends FilePlayer {
       for (File file : files) {
         if (file.isDirectory()) addMember(file);
       }
+    }
+  }
+
+  private final ArrayList<File> bookFiles = new ArrayList<>();
+
+  @Override
+  protected File nextFile () {
+    synchronized (bookFiles) {
+      if (bookFiles.isEmpty()) {
+        Set<File> encountered = new HashSet<>();
+
+        while (true) {
+          File book = super.nextFile();
+          if (book == null) return null;
+
+          if (encountered.contains(book)) return null;
+          encountered.add(book);
+
+          for (File file : book.listFiles()) {
+            if (!file.isFile()) continue;
+            if (!hasAudioExtension(file.getName())) continue;
+            bookFiles.add(file);
+          }
+
+          if (!bookFiles.isEmpty()) {
+            bookFiles.sort(
+              new Comparator<File>() {
+                @Override
+                public int compare (File file1, File file2) {
+                  return file1.getAbsolutePath().compareTo(file2.getAbsolutePath());
+                }
+              }
+            );
+
+            break;
+          }
+        }
+      }
+
+      return bookFiles.remove(0);
     }
   }
 }
