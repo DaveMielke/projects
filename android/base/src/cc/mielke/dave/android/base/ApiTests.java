002e216debc210615a3bb74e9874e781a9f30223
Dave Mielke <Dave@Mielke.cc>
2019-04-22 21:55:16 -0400

    Rename haveAndroidSDK() (in ApiTests) to haveLevel().

--- android/base/src/cc/mielke/dave/android/base/ApiTests.java
+++ android/base/src/cc/mielke/dave/android/base/ApiTests.java
@@ -6,37 +6,37 @@ public abstract class ApiTests {
   protected ApiTests () {
   }
 
-  public static boolean haveAndroidSDK (int sdk) {
-    return Build.VERSION.SDK_INT >= sdk;
+  public static boolean haveLevel (int level) {
+    return Build.VERSION.SDK_INT >= level;
   }
 
   public final static boolean haveJellyBean =
-    haveAndroidSDK(Build.VERSION_CODES.JELLY_BEAN);
+    haveLevel(Build.VERSION_CODES.JELLY_BEAN);
 
   public final static boolean haveJellyBeanMR1 =
-    haveAndroidSDK(Build.VERSION_CODES.JELLY_BEAN_MR1);
+    haveLevel(Build.VERSION_CODES.JELLY_BEAN_MR1);
 
   public final static boolean haveJellyBeanMR2 =
-    haveAndroidSDK(Build.VERSION_CODES.JELLY_BEAN_MR2);
+    haveLevel(Build.VERSION_CODES.JELLY_BEAN_MR2);
 
   public final static boolean haveKitkat =
-    haveAndroidSDK(Build.VERSION_CODES.KITKAT);
+    haveLevel(Build.VERSION_CODES.KITKAT);
 
   public final static boolean haveLollipop =
-    haveAndroidSDK(Build.VERSION_CODES.LOLLIPOP);
+    haveLevel(Build.VERSION_CODES.LOLLIPOP);
 
   public final static boolean haveLollipopMR1 =
-    haveAndroidSDK(Build.VERSION_CODES.LOLLIPOP_MR1);
+    haveLevel(Build.VERSION_CODES.LOLLIPOP_MR1);
 
   public final static boolean haveMarshmallow =
-    haveAndroidSDK(Build.VERSION_CODES.M);
+    haveLevel(Build.VERSION_CODES.M);
 
   public final static boolean haveNougat =
-    haveAndroidSDK(Build.VERSION_CODES.N);
+    haveLevel(Build.VERSION_CODES.N);
 
   public final static boolean haveNougatMR1 =
-    haveAndroidSDK(Build.VERSION_CODES.N_MR1);
+    haveLevel(Build.VERSION_CODES.N_MR1);
 
   public final static boolean haveOreo =
-    haveAndroidSDK(Build.VERSION_CODES.O);
+    haveLevel(Build.VERSION_CODES.O);
 }
