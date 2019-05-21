cdbef710fe120792e2c6b8b31d68fccda9d9e036
Dave Mielke <Dave@Mielke.cc>
2019-05-21 06:50:06 -0400

    Move the schedule's operand parsers inside their corresponding filter objects.

--- android/radio/src/cc/mielke/dave/android/radio/RadioSchedule.java
+++ android/radio/src/cc/mielke/dave/android/radio/RadioSchedule.java
@@ -64,10 +64,10 @@ public class RadioSchedule extends RadioComponent {
       throw new RuleException("program not defined");
     }
 
-    private abstract static class ValueParser {
+    private abstract static class OperandParser {
       private final String valueType;
 
-      protected ValueParser (String type) {
+      protected OperandParser (String type) {
         valueType = type;
       }
 
@@ -79,131 +79,7 @@ public class RadioSchedule extends RadioComponent {
       protected abstract String format (int value);
     }
 
-    private static class TimeParser extends ValueParser {
-      public TimeParser () {
-        super("time");
-      }
-
-      private final static long HOURS_PER_DAY = TimeUnit.DAYS.toHours(1);
-      private final static long MINUTES_PER_HOUR = TimeUnit.HOURS.toMinutes(1);
-      private final static long SECONDS_PER_MINUTE = TimeUnit.MINUTES.toSeconds(1);
-      private final static long MILLISECONDS_PER_SECOND = TimeUnit.SECONDS.toMillis(1);
-
-      private static class Time {
-        public int value = 0;
-      }
-
-      private final boolean add (Time time, Matcher matcher, int group, TimeUnit unit, long limit) {
-        String text = matcher.group(group);
-        if (text == null) return true;
-        if (text.isEmpty()) return true;
-
-        int value = Integer.valueOf(text, 10);
-        if (value < 0) return false;
-        if (value >= limit) return false;
-
-        time.value += unit.toMillis(value);
-        return true;
-      }
-
-      private final static String VALUE = "([0-9]{2})";
-      private final static String SEPARATOR = ":";
-
-      private final static Pattern pattern = Pattern.compile(
-        VALUE + SEPARATOR + VALUE + "(?:" + SEPARATOR + VALUE + ")?"
-      );
-
-      @Override
-      protected final Integer toInteger (String text) {
-        Matcher matcher = pattern.matcher(text);
-        if (!matcher.matches()) return null;
-
-        Time time = new Time();
-        if (!add(time, matcher, 1, TimeUnit.HOURS, HOURS_PER_DAY)) return null;
-        if (!add(time, matcher, 2, TimeUnit.MINUTES, MINUTES_PER_HOUR)) return null;
-        if (!add(time, matcher, 3, TimeUnit.SECONDS, SECONDS_PER_MINUTE)) return null;
-
-        return time.value;
-      }
-
-      @Override
-      protected final String format (int value) {
-        long milliseconds = value % MILLISECONDS_PER_SECOND;
-        value /= MILLISECONDS_PER_SECOND;
-
-        long seconds = value % SECONDS_PER_MINUTE;
-        value /= SECONDS_PER_MINUTE;
-
-        long minutes = value % MINUTES_PER_HOUR;
-        value /= MINUTES_PER_HOUR;
-
-        StringBuilder time = new StringBuilder();
-        time.append(String.format("%02d%s%02d", value, SEPARATOR, minutes));
-
-        if ((seconds != 0) || (milliseconds != 0)) {
-          time.append(String.format("%s%02d", SEPARATOR, seconds));
-
-          if (milliseconds != 0) {
-            time.append(String.format(".%03d", milliseconds));
-          }
-        }
-
-        return time.toString();
-      }
-    }
-
-    private static class DateParser extends ValueParser {
-      public DateParser () {
-        super("date");
-      }
-
-      private final static Pattern pattern = Pattern.compile(
-        "[1-9][0-9]?"
-      );
-
-      @Override
-      protected final Integer toInteger (String text) {
-        Matcher matcher = pattern.matcher(text);
-        if (!matcher.matches()) return null;
-
-        int date = Integer.valueOf(matcher.group(), 10);
-        if (date < 1) return null;
-        if (date > 31) return null;
-        return date;
-      }
-
-      @Override
-      protected final String format (int value) {
-        return String.format("%d", value);
-      }
-    }
-
-    private static class YearParser extends ValueParser {
-      public YearParser () {
-        super("year");
-      }
-
-      private final static Pattern pattern = Pattern.compile(
-        "[0-9]{4}"
-      );
-
-      @Override
-      protected final Integer toInteger (String text) {
-        Matcher matcher = pattern.matcher(text);
-        if (!matcher.matches()) return null;
-
-        int year = Integer.valueOf(matcher.group(), 10);
-        if (year < 1) return null;
-        return year;
-      }
-
-      @Override
-      protected final String format (int value) {
-        return String.format("%04d", value);
-      }
-    }
-
-    private abstract static class EnumerationParser extends ValueParser {
+    private abstract static class EnumerationParser extends OperandParser {
       private final Enum[] enumerationValues;
       private final Map<String, Integer> nameToValue = new HashMap<>();
 
@@ -240,33 +116,6 @@ public class RadioSchedule extends RadioComponent {
       }
     }
 
-    private static class DayParser extends EnumerationParser {
-      private static enum DAYS {
-        SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
-      }
-
-      public DayParser () {
-        super("day", DAYS.values());
-      }
-    }
-
-    private static class MonthParser extends EnumerationParser {
-      private static enum MONTHS {
-        JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE,
-        JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
-      }
-
-      public MonthParser () {
-        super("month", MONTHS.values());
-      }
-    }
-
-    private final static ValueParser timeParser = new TimeParser();
-    private final static ValueParser dateParser = new DateParser();
-    private final static ValueParser yearParser = new YearParser();
-    private final static ValueParser dayParser = new DayParser();
-    private final static ValueParser monthParser = new MonthParser();
-
     private abstract static class Filter {
       private static class Range {
         public final int from;
@@ -279,22 +128,22 @@ public class RadioSchedule extends RadioComponent {
       }
 
       private final List<Range> filterRanges = new LinkedList<>();
-      private final ValueParser valueParser;
+      private final OperandParser operandParser;
 
-      protected Filter (ValueParser parser) {
-        valueParser = parser;
+      protected Filter (OperandParser parser) {
+        operandParser = parser;
       }
 
-      public final ValueParser getParser () {
-        return valueParser;
+      public final OperandParser getOperandParser () {
+        return operandParser;
       }
 
-      public final String getType () {
-        return getParser().getType();
+      public final String getOperandType () {
+        return getOperandParser().getType();
       }
 
       public final String format (int from, int to) {
-        ValueParser parser = getParser();
+        OperandParser parser = getOperandParser();
         String string = parser.format(from);
         if (to != from) string += '-' + parser.format(to);
         return string;
@@ -307,7 +156,7 @@ public class RadioSchedule extends RadioComponent {
       public final void addRange (int from, int to) throws RuleException {
         if (to < from) {
           throw new RuleException(
-            "inverse range: %s: %s", getType(), format(from, to)
+            "inverse range: %s: %s", getOperandType(), format(from, to)
           );
         }
 
@@ -315,7 +164,7 @@ public class RadioSchedule extends RadioComponent {
           if ((from <= range.to) && (to >= range.from)) {
             throw new RuleException(
               "overlapping ranges: %s: %s & %s",
-              getType(), format(range), format(from, to)
+              getOperandType(), format(range), format(from, to)
             );
           }
         }
@@ -325,43 +174,198 @@ public class RadioSchedule extends RadioComponent {
     }
 
     private static class TimeFilter extends Filter {
+      private static class Parser extends OperandParser {
+        public Parser () {
+          super("time");
+        }
+
+        private final static long HOURS_PER_DAY = TimeUnit.DAYS.toHours(1);
+        private final static long MINUTES_PER_HOUR = TimeUnit.HOURS.toMinutes(1);
+        private final static long SECONDS_PER_MINUTE = TimeUnit.MINUTES.toSeconds(1);
+        private final static long MILLISECONDS_PER_SECOND = TimeUnit.SECONDS.toMillis(1);
+
+        private static class Time {
+          public int value = 0;
+        }
+
+        private final boolean add (Time time, Matcher matcher, int group, TimeUnit unit, long limit) {
+          String text = matcher.group(group);
+          if (text == null) return true;
+          if (text.isEmpty()) return true;
+
+          int value = Integer.valueOf(text, 10);
+          if (value < 0) return false;
+          if (value >= limit) return false;
+
+          time.value += unit.toMillis(value);
+          return true;
+        }
+
+        private final static String VALUE = "([0-9]{2})";
+        private final static String SEPARATOR = ":";
+
+        private final static Pattern pattern = Pattern.compile(
+          VALUE + SEPARATOR + VALUE + "(?:" + SEPARATOR + VALUE + ")?"
+        );
+
+        @Override
+        protected final Integer toInteger (String text) {
+          Matcher matcher = pattern.matcher(text);
+          if (!matcher.matches()) return null;
+
+          Time time = new Time();
+          if (!add(time, matcher, 1, TimeUnit.HOURS, HOURS_PER_DAY)) return null;
+          if (!add(time, matcher, 2, TimeUnit.MINUTES, MINUTES_PER_HOUR)) return null;
+          if (!add(time, matcher, 3, TimeUnit.SECONDS, SECONDS_PER_MINUTE)) return null;
+
+          return time.value;
+        }
+
+        @Override
+        protected final String format (int value) {
+          long milliseconds = value % MILLISECONDS_PER_SECOND;
+          value /= MILLISECONDS_PER_SECOND;
+
+          long seconds = value % SECONDS_PER_MINUTE;
+          value /= SECONDS_PER_MINUTE;
+
+          long minutes = value % MINUTES_PER_HOUR;
+          value /= MINUTES_PER_HOUR;
+
+          StringBuilder time = new StringBuilder();
+          time.append(String.format("%02d%s%02d", value, SEPARATOR, minutes));
+
+          if ((seconds != 0) || (milliseconds != 0)) {
+            time.append(String.format("%s%02d", SEPARATOR, seconds));
+
+            if (milliseconds != 0) {
+              time.append(String.format(".%03d", milliseconds));
+            }
+          }
+
+          return time.toString();
+        }
+      }
+
+      private final static OperandParser parser = new Parser();
+
       public TimeFilter () {
-        super(timeParser);
-      }
-    }
-
-    private static class DateFilter extends Filter {
-      public DateFilter () {
-        super(dateParser);
-      }
-    }
-
-    private static class YearFilter extends Filter {
-      public YearFilter () {
-        super(yearParser);
+        super(parser);
       }
     }
 
     private static class DayFilter extends Filter {
+      private static class Parser extends EnumerationParser {
+        private static enum DAYS {
+          SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+        }
+
+        public Parser () {
+          super("day", DAYS.values());
+        }
+      }
+
+      private final static OperandParser parser = new Parser();
+
       public DayFilter () {
-        super(dayParser);
+        super(parser);
+      }
+    }
+
+    private static class DateFilter extends Filter {
+      private static class Parser extends OperandParser {
+        public Parser () {
+          super("date");
+        }
+
+        private final static Pattern pattern = Pattern.compile(
+          "[1-9][0-9]?"
+        );
+
+        @Override
+        protected final Integer toInteger (String text) {
+          Matcher matcher = pattern.matcher(text);
+          if (!matcher.matches()) return null;
+
+          int date = Integer.valueOf(matcher.group(), 10);
+          if (date < 1) return null;
+          if (date > 31) return null;
+          return date;
+        }
+
+        @Override
+        protected final String format (int value) {
+          return String.format("%d", value);
+        }
+      }
+
+      private final static OperandParser parser = new Parser();
+
+      public DateFilter () {
+        super(parser);
       }
     }
 
     private static class MonthFilter extends Filter {
+      private static class Parser extends EnumerationParser {
+        private static enum MONTHS {
+          JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE,
+          JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER;
+        }
+
+        public Parser () {
+          super("month", MONTHS.values());
+        }
+      }
+
+      private final static OperandParser parser = new Parser();
+
       public MonthFilter () {
-        super(monthParser);
+        super(parser);
+      }
+    }
+
+    private static class YearFilter extends Filter {
+      private static class Parser extends OperandParser {
+        public Parser () {
+          super("year");
+        }
+
+        private final static Pattern pattern = Pattern.compile(
+          "[0-9]{4}"
+        );
+
+        @Override
+        protected final Integer toInteger (String text) {
+          Matcher matcher = pattern.matcher(text);
+          if (!matcher.matches()) return null;
+
+          int year = Integer.valueOf(matcher.group(), 10);
+          if (year < 1) return null;
+          return year;
+        }
+
+        @Override
+        protected final String format (int value) {
+          return String.format("%04d", value);
+        }
+      }
+
+      private final static OperandParser parser = new Parser();
+
+      public YearFilter () {
+        super(parser);
       }
     }
 
     private final Filter timeFilter = new TimeFilter();
-    private final Filter dateFilter = new DateFilter();
-    private final Filter yearFilter = new YearFilter();
     private final Filter dayFilter = new DayFilter();
+    private final Filter dateFilter = new DateFilter();
     private final Filter monthFilter = new MonthFilter();
+    private final Filter yearFilter = new YearFilter();
 
     private final Filter[] allFilters = new Filter[] {
-      timeFilter, dateFilter, yearFilter, dayFilter, monthFilter
+      timeFilter, dayFilter, dateFilter, monthFilter, yearFilter
     };
 
     private final void addRange (String operand) throws RuleException {
@@ -380,7 +384,7 @@ public class RadioSchedule extends RadioComponent {
       }
 
       for (Filter filter : allFilters) {
-        ValueParser parser = filter.getParser();
+        OperandParser parser = filter.getOperandParser();
         Integer from = parser.toInteger(start);
         if (from == null) continue;
         Integer to;
